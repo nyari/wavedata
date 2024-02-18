cea871f664687b19846257b989477930bdba0be6
Nyári-Kovács, Dávid Tamás <dev@dnyari.info>
2024-02-18 15:21:12 +0100

    Implement preamble on decoding

--- src/encodings/dec.rs
+++ src/encodings/dec.rs
@@ -11,7 +11,7 @@ pub mod nrzi {
 
     #[derive(Debug, Clone)]
     enum NRZIState {
-        Begin,
+        Begin(u8),
         Bit(usize),
         Done(usize),
     }
@@ -20,18 +20,39 @@ pub mod nrzi {
         stuff_bit_after: usize,
         payload: BitVec,
         frame_offset: usize,
+        preamble: u8,
     }
 
     impl NRZI {
-        pub fn parse(frame: &[Transition], bit_stuffing: usize) -> Result<Self, Error> {
+        pub fn parse(
+            frame: &[Transition],
+            bit_stuffing: usize,
+            preamble: u8,
+        ) -> Result<Self, Error> {
             let mut result = BitVec::new();
-            let mut sm = NRZIState::Begin;
+            let mut sm = NRZIState::Begin(0);
+            let mut sof = 0u8;
 
             for (idx, ts) in frame.iter().enumerate() {
                 sm = match (sm.clone(), ts) {
-                    (NRZIState::Begin, Transition::Noise(_)) => Ok(NRZIState::Begin),
-                    (NRZIState::Begin, Transition::Rising) => Ok(NRZIState::Bit(0)),
-                    (NRZIState::Begin, _) => Err(Error::IncorrectStartOfFrame),
+                    (NRZIState::Begin(0), Transition::Noise(_)) => Ok(NRZIState::Begin(0)),
+                    (NRZIState::Begin(value), Transition::Rising) if value % 2 == 0 => {
+                        sof += 1;
+                        if sof < preamble {
+                            Ok(NRZIState::Begin(sof))
+                        } else {
+                            Ok(NRZIState::Bit(0))
+                        }
+                    },
+                    (NRZIState::Begin(value), Transition::Falling) if value % 2 == 1 => {
+                        sof += 1;
+                        if sof < preamble {
+                            Ok(NRZIState::Begin(sof))
+                        } else {
+                            Ok(NRZIState::Bit(0))
+                        }
+                    },
+                    (NRZIState::Begin(_), _) => Err(Error::IncorrectStartOfFrame),
                     (NRZIState::Bit(hold_count), Transition::Hold(hold_length)) => {
                         if *hold_length + hold_count <= bit_stuffing {
                             for _ in 0..*hold_length {
@@ -66,6 +87,7 @@ pub mod nrzi {
                     stuff_bit_after: bit_stuffing,
                     payload: result,
                     frame_offset,
+                    preamble,
                 })
             } else {
                 panic!("Internal error")
@@ -111,10 +133,50 @@ pub mod nrzi {
                 Transition::Noise(1),
             ];
 
-            let result = NRZI::parse(&input, 4).unwrap();
+            let result = NRZI::parse(&input, 4, 0).unwrap();
 
             assert_eq!("ABCD".as_bytes(), result.payload());
-            assert_eq!(result.frame_offset, 26);
+            assert_eq!(result.frame_offset, input.len());
+        }
+        #[test]
+        fn nrzi_test_2() {
+            let input = [
+                Transition::Rising,
+                Transition::Falling,
+                Transition::Rising,
+                Transition::Falling,
+                Transition::Rising,
+                Transition::Hold(1),
+                Transition::Falling,
+                Transition::Hold(4),
+                Transition::Rising,
+                Transition::Hold(1),
+                Transition::Falling,
+                Transition::Hold(1),
+                Transition::Rising,
+                Transition::Hold(4),
+                Transition::Falling,
+                Transition::Rising,
+                Transition::Hold(2),
+                Transition::Falling,
+                Transition::Hold(4),
+                Transition::Rising,
+                Transition::Falling,
+                Transition::Rising,
+                Transition::Hold(1),
+                Transition::Falling,
+                Transition::Hold(3),
+                Transition::Rising,
+                Transition::Hold(2),
+                Transition::Falling,
+                Transition::Hold(4),
+                Transition::Noise(1),
+            ];
+
+            let result = NRZI::parse(&input, 4, 5).unwrap();
+
+            assert_eq!("ABCD".as_bytes(), result.payload());
+            assert_eq!(result.frame_offset, input.len());
         }
     }
 }
